231e79018640632c74f8d83b64ce9f3b4532412c
c-bata <contact@c-bata.link>
2015-10-20 12:00:34 +0900

    Add error route

--- flask_tpl/__init__.py
+++ flask_tpl/__init__.py
@@ -23,8 +23,19 @@ def create_app():
                 app.logger.warning(
                     'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
                     % (query.statement, query.parameters, query.duration,
-                       query.context))
+                       query.context)
+                )
         return response
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        """Return a custom 404 error."""
+        return 'Sorry, Nothing at this URL.', 404
+
+    @app.errorhandler(500)
+    def page_not_found(e):
+        """Return a custom 500 error."""
+        return 'Sorry, unexpected error: {}'.format(e), 500
 
     return app
 
