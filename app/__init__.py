b927f1087bb84f35c91ceb38b526bd528e9a8170
c-bata <contact@c-bata.link>
2015-10-21 17:49:06 +0900

    Refactor using pep8

--- app/__init__.py
+++ app/__init__.py
@@ -12,20 +12,23 @@ def create_app():
     db.init_app(app)
     Migrate(app, db)
 
+    # Register the blueprint applications
     from .views.cms import cms as cms_blueprint
     app.register_blueprint(cms_blueprint, url_prefix='/cms')
 
     from .views.api_v1 import api as api_v1_blueprint
     app.register_blueprint(api_v1_blueprint, url_prefix='/api/v1')
 
+    # Define special routes
     @app.after_request
     def after_request(response):
         for query in get_debug_queries():
             if query.duration >= app.config['SLOW_DB_QUERY_TIME']:
                 app.logger.warning(
-                    'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n'
-                    % (query.statement, query.parameters, query.duration,
-                       query.context)
+                    'Slow query: %s\nParameters: %s\n'
+                    'Duration: %fs\nContext: %s\n'
+                    % (query.statement, query.parameters,
+                       query.duration, query.context)
                 )
         return response
 
