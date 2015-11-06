84c19cfdc4ccc86ff884b2fb561085d6f1b32a9d
c-bata <contact@c-bata.link>
2015-11-06 15:26:57 +0900

    Update models

--- app/models.py
+++ app/models.py
@@ -5,8 +5,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), index=True)
-    password_hash = db.Column(db.String(128))
+    name = db.Column(db.String(128), index=True, nullable=False)
+    email = db.Column(db.String(128), index=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    records = db.relationship('Record', backref='user')
 
     @property
     def password(self):
@@ -27,4 +29,28 @@ class User(db.Model):
         return {
             'id': self.id,
             'name': self.name,
+        }
+
+
+class Record(db.Model):
+    __tablename__ = 'records'
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.Date, nullable=False)
+    begin_time = db.Column(db.DateTime, index=True, nullable=False)
+    finish_time = db.Column(db.DateTime, index=True, nullable=False)
+    contents = db.Column(db.String(256), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Record %r %r>' % (self.user_id, self.day)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'day': self.day,
+            'begin_time': self.begin_time,
+            'finish_time': self.finish_time,
+            'contents': self.contents,
+            'user_id': self.user_id
         }
